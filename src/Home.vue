8b490617774fa30e5ff53344107280d060a39517
Roger <luojie@w-ew.com>
2019-01-17 17:29:31 +0800

    1

--- src/Home.vue
+++ src/Home.vue
@@ -7,15 +7,15 @@
         width="24vh"
         style="background-color: white;width:200px"
       >
-        <el-menu :default-openeds="['1', '3']">
+        <el-menu :default-openeds="['1', '3']" @select="handleSelect">
           <el-submenu index="1">
             <template slot="title">
               <i class="el-icon-message"></i>导航一
             </template>
             <el-menu-item-group>
               <template slot="title">分组一</template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
+              <el-menu-item index="test1">trst1</el-menu-item>
+              <el-menu-item index="test2">test2</el-menu-item>
             </el-menu-item-group>
             <el-menu-item-group title="分组2">
               <el-menu-item index="1-3">选项3</el-menu-item>
@@ -63,16 +63,14 @@
       </el-aside>
       <el-container>
         <!--  头部  !-->
-        <el-header style="text-align: left; font-size: 24px; background-color:#303133;height:80px;width:100%">
-          <p
-            style="position: absolute; top:-6px;left: 42px;font-size: 23px;font-family: 微软雅黑;font-weight: bold; color: #f5f7f9; top: -10px;"
-          >智能巡检系统</p>
+        <el-header style="text-align: left; font-size: 24px; background-color:#606266;height:80px;width:100%">
+          <p style="position: absolute; top:-6px;left: 42px;font-size: 23px;font-family: 微软雅黑;font-weight: bold; color: #f5f7f9; top: -10px;">智能巡检系统</p>
           <el-menu
             :default-active="activeIndex"
             class="el-menu-demo"
             mode="horizontal"
             @select="handleSelect"
-            background-color="#303133"
+            background-color="#606266"
             text-color="#fff"
             active-text-color="#ffd04b"
           >
@@ -83,43 +81,100 @@
             <el-menu-item index="5">5</el-menu-item>
             <el-menu-item index="6">6</el-menu-item>
           </el-menu>
-          <el-dropdown style="position:fixed;right:25px;top:10px">
-            <!-- <el-button type="primary" style="background-color:#ffd04b;"  icon="el-icon-setting">
-            </el-button>
-            <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item icon="el-icon-search"></el-dropdown-item>
-              <el-dropdown-item>皮肤</el-dropdown-item>
-              <el-dropdown-item>设置</el-dropdown-item>
-            </el-dropdown-menu> -->
+          <div style="position:fixed;right:25px;top:10px">
             <i class="el-icon-info"></i>&nbsp;&nbsp;
             <i class="el-icon-view"></i>&nbsp;&nbsp;
             <i class="el-icon-setting"></i>
-          </el-dropdown>
+          </div>
         </el-header>
         <!--  主要区域内容  !-->
-        <el-main style="right:200px;width:1200px;position:relative;left:230px;padding-left: 0px;">
-          <el-breadcrumb>
-            <el-breadcrumb-item>首页</el-breadcrumb-item>
-            <el-breadcrumb-item>活动管理</el-breadcrumb-item>
-            <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-            <el-breadcrumb-item>活动详情</el-breadcrumb-item>
-          </el-breadcrumb>
+        <el-main style="right:200px;position:relative;left:230px;padding-left: 0px;">
+          <el-tabs v-model="editableTabsValue" type="card" @tab-click="view" editable @edit="handleTabsEdit"  >
+            <el-tab-pane
+              :key="item.name"
+              v-for="(item) in editableTabs"
+              :label="item.title"
+              :name="item.name"
+              lazy
+            >
+            </el-tab-pane>
+          </el-tabs>
           <br>
           <div class="layout-content">
-                    <div class="layout-content-main">内容区域</div>
-                </div>
+            <div class="layout-content-main" style="width:60vh;height:70vh"><router-view></router-view></div>
+          </div>
         </el-main>
       </el-container>
     </el-container>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      activeIndex: "1",
+
+      editableTabsValue: '1',
+      editableTabs: [{
+        title: '首页',
+        name: '1',
+      }],
+      tabIndex: 1
+    };
+  },
+  methods: {
+    handleSelect(key) {
+      console.log(this.editableTabs)
+      let check = true;
+      for(let i = 0; i< this.editableTabs.length; i++){
+        if(this.editableTabs[i].title == key){
+          check = false
+        }
+      }
+      if(check){
+        this.handleTabsEdit(key,'add')
+        this.$router.push('/home/'+key)
+      }
+      console.log(this.editableTabsValue)
+    },
+    handleTabsEdit(targetName, action) {
+        if (action === 'add') {
+          let newTabName = ++this.tabIndex + '';
+          this.editableTabs.push({
+            title: targetName,
+            name: '/home/'+targetName
+          });
+          this.editableTabsValue = newTabName;
+        }
+        if (action === 'remove') {
+          let tabs = this.editableTabs;
+          let activeName = this.editableTabsValue;
+          if (activeName === targetName) {
+            tabs.forEach((tab, index) => {
+              if (tab.name === targetName) {
+                let nextTab = tabs[index + 1] || tabs[index - 1];
+                if (nextTab) {
+                  activeName = nextTab.name;
+                }
+              }
+            });
+          }
+          this.editableTabsValue = activeName;
+          this.editableTabs = tabs.filter(tab => tab.name !== targetName);
+        }
+      },
+    view(tar){
+      this.$router.push(tar.name)
+    }
+  }
+};
+</script>
 
 <style>
-
-body{
-	margin: 0; 
-	padding: 0;
-	border: 0;
+body {
+  margin: 0;
+  padding: 0;
+  border: 0;
 }
 .el-header {
   background-color: #b3c0d1;
@@ -137,29 +192,15 @@ body{
   left: 180px;
   top: 20px;
 }
-.layout-content{
-        background-color:#D0D0D0;
-        min-height: 540px;
-        min-width: 1150px;
-        overflow: hidden;
-        border-radius: 4px;
-    }
-    .layout-content-main{
-        padding: 10px;
-    }
+.layout-content {
+  background-color: #d0d0d0;
+  min-height: 540px;
+  min-width: 1150px;
+  overflow: hidden;
+  border-radius: 4px;
+}
+.layout-content-main {
+  padding: 10px;
+}
 </style>
 
-<script>
-export default {
-  data() {
-    return {
-      activeIndex: "1"
-    };
-  },
-  methods: {
-    handleSelect(key, keyPath) {
-      console.log(key, keyPath);
-    }
-  }
-};
-</script>
--- src/main.js
+++ src/main.js
@@ -4,15 +4,35 @@ import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 import Home from './Home.vue'
 import Router from 'vue-router'
+import Login from './Login.vue'
 
 Vue.use(ElementUI)
 Vue.use(Router)
 
+import Test1 from './components/test1.vue'
+import Test2 from './components/test2.vue'
 const routes = [
   {
     path:'/',
+    name:'Login',
+    component:Login
+  },
+  {
+    path:'/home',
     name:'Home',
-    component:Home
+    component:Home,
+    children:[
+      {
+        path:'/home/test1',
+        name:'Test1',
+        component:Test1
+      },
+      {
+        path:'/home/test2',
+        name:'Test2',
+        component:Test2
+      }
+    ]
   }
 ]
 const router = new Router ({
